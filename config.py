8f5221e9e16b4c1a8c57ea0b1c29e2a605be1009
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-04-20 12:08:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '9176863'
+API_HASH = 'afff208ad0de11acfc946ca6dcd74aec'
+TOKEN = '6359152520:AAE7Oz_Jr5F6TGPJpR1cHPalqxbzuFp59rk'
